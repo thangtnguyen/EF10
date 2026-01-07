dc6a8c1375a802b20c6ae336e61eb469a86eeb8c
Thang Nguyen <thangnguyen0722@gmail.com>
2026-01-07 10:15:34 +0700

    Create the Configuration and Get It Loaded

--- EF_ConsoleProgram/Program.cs
+++ EF_ConsoleProgram/Program.cs
@@ -1,9 +1,28 @@
-﻿namespace EF_ConsoleProgram;
+﻿using EF_DBLibrary;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace EF_ConsoleProgram;
 
 class Program
 {
+    private static IConfigurationRoot _configuration;
+
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+        BuildConfiguration();
+        var connectionString = _configuration.GetConnectionString("EFDbConnection");
+        Console.WriteLine($"Connection String: {connectionString}");
+        //...additional code coming soon will go here
+        Console.WriteLine("done");
+    }
+
+    private static void BuildConfiguration()
+    {
+        var builder = new ConfigurationBuilder()
+                        .SetBasePath(Directory.GetCurrentDirectory())
+                        .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+        _configuration = builder.Build();
     }
 }
